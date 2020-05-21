a72ddc9efc2b50c0146a0830e6798917e4daa8c9
Tobias Käfer <6708974+kaefer3000@users.noreply.github.com>
2020-05-21 17:58:03 +0200

    tasks tc* are long-running

--- run-agent.sh
+++ run-agent.sh
@@ -10,11 +10,19 @@ echo "please run with task id as parameter, eg. ts1"
 exit 2
 fi
 
+beginswith() { case $2 in "$1"*) true;; *) false;; esac; }
+
+if beginswith tc $1 ; then
+LDFU_PARAM="-n"
+else
+LDFU_PARAM=""
+fi
+
 curl -X DELETE http://127.0.1.1:8080/sim
 
 curl -X PUT http://127.0.1.1:8080/sim --data-binary @../bbench-server/data/sim.ttl -Hcontent-type:text/turtle -v
 
-./ldfu.sh -p brick-*.n3 -p $1*/*.n3
+timeout 60s ./ldfu.sh -p brick-*.n3 -p $1*/*.n3 $LDFU_PARAM
 
 curl -X DELETE http://127.0.1.1:8080/sim
 
