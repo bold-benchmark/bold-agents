865ec6bab7bf6ee9329a6278885e6d70a3c36bb8
Tobias Käfer <6708974+kaefer3000@users.noreply.github.com>
2024-12-20 12:39:11 +0100

    cleanup for jakarta code

--- run-agent.sh
+++ run-agent.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 # Checking for BOLD server
-curl http://127.0.1.1:8080/ || { echo "No BOLD server running on localhost on port 8080" ; exit 3 ; }
+curl -s http://127.0.1.1:8080/ > /dev/null || { echo "No BOLD server running on 127.0.1.1 on port 8080" ; exit 3 ; }
 
 # Checking for agent runtime
 if [ ! -f "ldfu.sh" ] ; then
@@ -33,15 +33,18 @@ echo "option set: serial"
 fi
 
 
-# Cleaning up the BOLD server
-curl -X DELETE http://127.0.1.1:8080/sim
+## Cleaning up the BOLD server
+#curl -X DELETE http://127.0.1.1:8080/sim
+
+# Configuring a simulation
+curl -X PUT http://127.0.1.1:8080/gsp/sim --data-binary @sim.ttl -Hcontent-type:text/turtle -v
 
 # Starting a simulation
-curl -X PUT http://127.0.1.1:8080/sim --data-binary @sim.ttl -Hcontent-type:text/turtle -v
+curl -X POST http://127.0.1.1:8080/sim -v
 
 # Running the specified task on ldfu with a duration of a single run ~ 150s
 timeout 150s ./ldfu.sh -p brick-*.n3 -p $1*/*.n3 $LDFU_PARAM
 
-# Cleaning up the BOLD server
-curl -X DELETE http://127.0.1.1:8080/sim
+## Cleaning up the BOLD server
+#curl -X DELETE http://127.0.1.1:8080/sim
 
